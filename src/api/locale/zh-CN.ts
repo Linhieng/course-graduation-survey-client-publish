5197ee348bc23f75ffa931da988a3980f24d3e02
linhieng <790728740@qq.com>
2024-04-21 15:28:01 +0800

    locale

--- src/api/locale/zh-CN.ts
+++ src/api/locale/zh-CN.ts
@@ -1,3 +1,10 @@
 export default {
     'api.error.unknown': '未知错误',
+
+    'api.error.survey-link-wrong': '问卷填写链接错误',
+    'api.error.survey-not-exist': '不存在此问卷',
+    'api.error.survey-not-complete': '问卷未完成',
+    'api.error.survey-deleted': '问卷已经被删除',
+    'api.error.survey-stop': '问卷已经停止收集',
+    'api.error.survey-content-not-exist': '问卷内容不存在',
 };
